5503d1c446a189ec3497c82656773c71fe12e8de
SmKou <66178856+SmKou@users.noreply.github.com>
2023-08-28 22:35:43 -0700

    Add interface (tested)

--- src/index.js
+++ src/index.js
@@ -11,8 +11,10 @@ const problems_select = document.getElementById('problems');
 const user_input = document.getElementById('user_input')
 const solutions_section = document.getElementById('solutions');
 
-problems_select.addEventListener('change', e => {
-    const problem = e.value;
+problems_select.addEventListener('change', () => {
+    const problem = problems_select.value;
+    if (!problem)
+        return;
     const euler_problem = euler_problems.get(problem);
 
     const intro_div = document.createElement('div');
@@ -20,8 +22,9 @@ problems_select.addEventListener('change', e => {
     const h3 = document.createElement('h3');
     h3.append(document.createTextNode(euler_problem.problem_name));
     intro_div.append(h3);
-    const p = docucment.createElement('p');
+    const p = document.createElement('p');
     p.append(document.createTextNode(euler_problem.description));
+    p.append(document.createElement('br'));
     const a = document.createElement('a');
     a.href = euler_problem.url;
     a.target = "_blank";
@@ -31,7 +34,7 @@ problems_select.addEventListener('change', e => {
     solutions_section.append(intro_div);
 
     const input_value = user_input.value;
-    const input = input ? euler_problem.check_input_value(input_value) : euler_problem.default;
+    const input = input_value ? euler_problem.check_input_value(input_value) : euler_problem.default;
     
     const solutions = euler_problem.solutions;
     for (let i = 0; i < solutions.length; i++) {
@@ -41,16 +44,20 @@ problems_select.addEventListener('change', e => {
 
         const div = document.createElement('div');
         const p_ttl = document.createElement('p');
-        p_ttl.append(document.createTextNode(`Solution ${i}:`));
+        p_ttl.style.fontWeight = "bold";
+        p_ttl.append(document.createTextNode(`Solution ${i + 1}:`));
         const p_desc = document.createElement('p');
         p_desc.append(document.createTextNode(euler_problem.logic[i]));
         p_desc.append(document.createElement('br'));
         p_desc.append(document.createTextNode(`Time: ${stop - start} ms`));
-        const p_res = document.createElement('p');
-        p_res.append(document.createTextNode(euler_problem.present(result)));
+        const details_res = document.createElement('details');
+        const summary = document.createElement('summary');
+        summary.append(document.createTextNode('Result'));
+        details_res.append(summary);
+        details_res.append(document.createTextNode(euler_problem.present(result)));
         div.append(p_ttl);
         div.append(p_desc);
-        div.append(p_res);
+        div.append(details_res);
         solutions_section.append(div);
     }
 });
--- src/js/1_Mult3or5.js
+++ src/js/1_Mult3or5.js
@@ -5,7 +5,7 @@ const ans_1 = n => {
             numbers.splice(i, 1);
     return numbers;
 }
-const ans_1_desc = "Create an array populated with values incrementing from 3 to n by 1. Loop through the array backwards, splicing elements that can be divided by 3 and/or 5, then return the array."
+const ans_1_desc = "Create an array populated with values incrementing from 3 to n by 1. Loop through the array backwards, splicing elements that can be divided by 3 and/or 5, then return the array.";
 
 const ans_2 = n => {
     const multiples = [];
@@ -14,7 +14,7 @@ const ans_2 = n => {
             multiples.push(i);
     return multiples;
 }
-const ans_2_desc = "Create an empty array. Loop from 1 to n, and if the index is divisible by either 3 and/or 5, push the index into the array, then return the array."
+const ans_2_desc = "Create an empty array. Loop from 1 to n, and if the index is divisible by either 3 and/or 5, push the index into the array, then return the array.";
 
 const ans_3 = n => {
     const multiples = [];
@@ -25,7 +25,7 @@ const ans_3 = n => {
             multiples.push(5 * i);
     return multiples;
 }
-const ans_3_desc = "Create an empty array. Loop through multiples of 3 until 3 multipled by the index is greater than or equal to n, and push each value into the array, then loop through multiples of 5 until 5 multiplied by the index is greater than or equal to n, but skip that are divisible by three to avoid duplicates. Return the array."
+const ans_3_desc = "Create an empty array. Loop through multiples of 3 until 3 multipled by the index is greater than or equal to n, and push each value into the array, then loop through multiples of 5 until 5 multiplied by the index is greater than or equal to n, but skip that are divisible by three to avoid duplicates. Return the array.";
 
 const ans_4 = n => {
     const multiples = [];
@@ -41,7 +41,7 @@ const ans_4 = n => {
     }
     return multiples;
 }
-const ans_4_desc = "Create an empty array. Calculate the number of multiples of 3 to n, exclusive, then loop this number of times and push the result of 3 multiplied by the index into the array. Calculate the number of multiples of 5 to n, exclusive, then loop this number of times and push the result, but if the index is a multiple of 3, do not push, continue. Return the array."
+const ans_4_desc = "Create an empty array. Calculate the number of multiples of 3 to n, exclusive, then loop this number of times and push the result of 3 multiplied by the index into the array. Calculate the number of multiples of 5 to n, exclusive, then loop this number of times and push the result, but if the index is a multiple of 3, do not push, continue. Return the array.";
 
 const ans_5 = (n, i = 1, arr = []) => {
     if (i * 3 >= n && i * 5 >= n)
@@ -52,25 +52,25 @@ const ans_5 = (n, i = 1, arr = []) => {
         arr.push(i * 3);
     return ans_5(n, ++i, arr);
 }
-const ans_5_desc = "On first call, declare defaults: i = 1 and arr = []. On each iteration, check if both the index multiplied by 3 and multiplied by 5 are greater than or equal to n. Then, if the index multiplied by 5 is less than n, push the multiple into the array. If the index multiplied by 3 is less than n and not divisible by 5 as well, push the multiple into the array. Return this function with n, i incremented by 1, and the array."
+const ans_5_desc = "On first call, declare defaults: i = 1 and arr = []. On each iteration, check if both the index multiplied by 3 and multiplied by 5 are greater than or equal to n. Then, if the index multiplied by 5 is less than n, push the multiple into the array. If the index multiplied by 3 is less than n and not divisible by 5 as well, push the multiple into the array. Return this function with n, i incremented by 1, and the array.";
 
-export default function Mult3or5() { 
-    return {
-        problem_name: "Multiples of 3 or 5",
-        description: "If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6, and 9. The sum of these multiples is 23.\nFind the sum of all multiples of 3 or 5 below 1000.",
-        url: "https://projecteuler.net/problem=1",
-        solutions: [ans_1, ans_2, ans_3, ans_4, ans_5],
-        logic: [ans_1_desc, ans_2_desc, ans_3_desc, ans_4_desc, ans_5_desc],
-        present: (result) => result.toString(),
-        default: 1000,
-        check_input_valid: (v) => {
-            try {
-                const n = parseInt(v);
-                if (n < 1)
-                    return 1000;
-                return true;
-            }
-            catch { return 1000; }
+const Mult3or5 = {
+    problem_name: "Multiples of 3 or 5",
+    description: "If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6, and 9. The sum of these multiples is 23.\nFind the sum of all multiples of 3 or 5 below 1000.",
+    url: "https://projecteuler.net/problem=1",
+    solutions: [ans_1, ans_2, ans_3, ans_4, ans_5],
+    logic: [ans_1_desc, ans_2_desc, ans_3_desc, ans_4_desc, ans_5_desc],
+    present: (result) => result.join(", "),
+    default: 1000,
+    check_input_valid: (v) => {
+        try {
+            const n = parseInt(v);
+            if (n < 1)
+                return 1000;
+            return true;
         }
-    };
+        catch (err) { return 1000; }
+    }
 }
+
+export default Mult3or5;
